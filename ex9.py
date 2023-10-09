3be88c5629f585832359c6baa2f3391cd7d31fca
HitzCommitted <hitzcommitted@gmail.com>
2023-10-09 20:58:15 +0300

    basics of args and kwargs

--- ex9.py
+++ ex9.py
@@ -2,4 +2,26 @@ def myFunc(*args):
     return sum((args))
 
 
-print(myFunc(1, 2, 3))
+print(myFunc(1, 100, 101))
+
+
+def myFunc(**kwargs):
+    if "name" in kwargs:
+        print(f"My name is {kwargs['name']}")
+    else:
+        print("I did not find any name")
+
+
+myFunc(name="alex", age=32)
+myFunc(name="dee", age=33)
+myFunc(name="soi", age=28)
+myFunc(age=76)
+
+
+def myFunc(*args, **kwargs):
+    print(
+        f"I would like {args[0]} students to eat {kwargs['meal']} with {kwargs['fruit']} every {kwargs['day']} at {kwargs['time']}"
+    )
+
+
+myFunc(10, 15, 49, 18, fruit="apples", meal="pizza", day="monday", time="noon")
