33c36f092e7df2edc0da4c59888d735dfbc7a00a
HitzCommitted <hitzcommitted@gmail.com>
2023-10-12 09:09:50 +0300

    done

--- ex21.py
+++ ex21.py
@@ -3,13 +3,14 @@ Lambda Expressions Map and Filter
 """
 
 
+# Map function
 def squares(num):
     return num**2
 
 
 my_nums = [1, 3, 2, 4, 5, 6]
 
-print(list(map(squares, my_nums)))
+# print(list(map(squares, my_nums)))
 
 
 def splicer(my_string):
@@ -21,4 +22,33 @@ def splicer(my_string):
 
 names = ["Alex", "Diana", "Shiro", "Tomm", "Brad"]
 
-print(list(map(splicer, names)))
+# print(list(map(splicer, names)))
+
+
+# Filter function
+def check_even(num):
+    return num % 2 == 0
+
+
+my_nums = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# print(list(filter(check_even, my_nums)))
+
+
+# Lambda Expressions
+# without lambda
+def new_square(num):
+    result = num**2
+    return result
+
+
+# with lambda
+square = lambda num: num**2
+
+# print(square(5))
+
+print(list(map(lambda num: num**2, my_nums)))
+
+print(list(filter(lambda num: num % 2 == 0, my_nums)))
+
+print(list(map(lambda name: name[0], names)))
