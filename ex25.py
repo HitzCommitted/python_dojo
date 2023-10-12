c91e81223bca920ea13f36e8af629c23cd515d39
HitzCommitted <hitzcommitted@gmail.com>
2023-10-12 16:17:42 +0300

    working

---
+++ ex25.py
@@ -0,0 +1,18 @@
+def up_low(my_string):
+    upper = 0
+    lower = 0
+
+    for lett in my_string:
+        if lett.isupper():
+            upper += 1
+        elif lett.islower():
+            lower += 1
+
+    count_string = f"No. of Upper case characters : {upper} \nNo. of Lower case characters : {lower}"
+
+    return count_string
+
+
+input_string = "Hello Mr. Rogers, how are you this fine Tuesday?"
+
+print(up_low(input_string))
